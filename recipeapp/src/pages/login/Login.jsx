f7ade6c8a10fe0cfcd4146d63f93cd5112c4e105
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-11 20:01:43 +0300

    created project skeleton.

---
+++ recipeapp/src/pages/login/Login.jsx
@@ -0,0 +1,12 @@
+import { LoginStyled } from "./Login.styled"; 
+
+const Login = () => {
+  return (
+    <LoginStyled>
+        <div>Login</div>
+    </LoginStyled>
+    
+  )
+}
+
+export default Login;
---
+++ recipeapp/src/pages/login/Login.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const LoginStyled = styled.body`
+    width: 100vw;
+    height: 100vh;
+    background-image: url("https://picsum.photos/1600/900");
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+`
