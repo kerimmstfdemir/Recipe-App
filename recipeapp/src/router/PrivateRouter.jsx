0436f89c0d951e5ef5285e00dc36d32e33c96c57
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-12 20:11:15 +0300

    defined reducer states (context api)

--- recipeapp/src/router/PrivateRouter.jsx
+++ recipeapp/src/router/PrivateRouter.jsx
@@ -1,7 +1,15 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useEffect, useReducer } from "react";
+import { initalState, reducer } from "../reducer";
 
 const PrivateRouter = () => {
-    const loginInformation = false; 
+    const [state, dispatch] = useReducer(reducer, initalState);
+    const { loginInformation } = state;
+    useEffect(() => {
+        dispatch({type:"LOGIN"})
+    }, []);
+    
+    // const loginInformation = false; 
   return (
     <div>
         {loginInformation ? <Navigate to={"/home"} /> : <Outlet />}
