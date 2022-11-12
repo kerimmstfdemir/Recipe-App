7effdc7351db388a89f79bb3ad0043fbbcb73ef6
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-12 10:24:52 +0300

    modified login page.

--- recipeapp/src/pages/login/Login.jsx
+++ recipeapp/src/pages/login/Login.jsx
@@ -1,12 +1,29 @@
-import { LoginStyled } from "./Login.styled"; 
+import { LoginStyled, LoginDivStyled, LoginImgStyled, LoginPStyled } from "./Login.styled";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import meal2 from "../../assets/meal2.svg";
 
 const Login = () => {
   return (
     <LoginStyled>
-        <div>Login</div>
+      <LoginDivStyled>
+        <LoginImgStyled src={meal2}></LoginImgStyled>
+        <LoginPStyled>RECIPE APP</LoginPStyled>
+        <Form className="d-flex flex-column">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="text" placeholder="USERNAME" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="PASSWORD" />
+          </Form.Group>
+
+          <Button type="submit" variant="primary">LOGIN</Button>
+          
+        </Form>
+      </LoginDivStyled>
     </LoginStyled>
-    
-  )
-}
+  );
+};
 
 export default Login;
--- recipeapp/src/pages/login/Login.styled.jsx
+++ recipeapp/src/pages/login/Login.styled.jsx
@@ -3,8 +3,33 @@ import styled from "styled-components";
 export const LoginStyled = styled.body`
     width: 100vw;
     height: 100vh;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif ;
     background-image: url("https://picsum.photos/1600/900");
     background-repeat: no-repeat;
     background-size: cover;
-    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const LoginDivStyled = styled.div`
+    border: 4px solid white;
+    border-radius: 50%;
+    background-color: green;
+    opacity: 0.80;
+    width: 30rem;
+    height: 30rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const LoginImgStyled = styled.img`
+    width: 25%;
+    margin-bottom: 1rem;
+`
+
+export const LoginPStyled = styled.p`
+    font-size: 2rem;
 `
