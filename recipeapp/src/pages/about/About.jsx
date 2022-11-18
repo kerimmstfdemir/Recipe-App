cefe3912b137d2820c2a0573589c15e14adc20d6
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-18 10:21:56 +0300

    modified about page

--- recipeapp/src/pages/about/About.jsx
+++ recipeapp/src/pages/about/About.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import coding from "../../assets/coding.svg";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { AboutStyledDiv, AboutStyledH2 } from "./About.styled"; 
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+    
+    <AboutStyledDiv className="d-flex flex-row align-items-center">
+      <Card style={{ width: "80vw", height:"70vh", margin:"1rem 7rem", display:"flex", flexDirection:"column", alignItems:"center", }}>
+      <AboutStyledH2>About Software Developer</AboutStyledH2>
+        <Card.Img variant="top" src={coding} style={{width:"50%"}}/>
+        <Card.Body style={{display:"flex", flexDirection:"column", justifyContent:"center", height:"100%"}}>
+          <Card.Title className="text-center">Kerim Mustafa DEMİR</Card.Title>
+          <Card.Text style={{height:"100%"}}>
+            <p> Hi everyone 👋 </p>
+            <p> I'm Kerim. </p>
+            <p> I'm interested in Full Stack Development and continue to learn languages in this field. </p>
+            <p>You can contact me from the following links.</p>
+            <p>Happy Coding...</p>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </AboutStyledDiv>
+    </>
+  );
+};
 
 export default About;
---
+++ recipeapp/src/pages/about/About.styled.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const AboutStyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const AboutStyledH2 = styled.h2`
+  font-size: 2rem;
+  text-align: center;
+  padding: 2rem 2rem 1rem 2rem;
+`;
