75028fea86865dd0d2de9fe97c6ceb7bdf81842e
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-18 17:31:07 +0300

    modified and updated .about page

--- recipeapp/src/pages/about/About.jsx
+++ recipeapp/src/pages/about/About.jsx
@@ -1,29 +1,44 @@
 import coding from "../../assets/coding.svg";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import { AboutStyledDiv, AboutStyledH2 } from "./About.styled"; 
+import { AboutStyledDiv, AboutStyledH2, AboutStyledContactLinks } from "./About.styled";
+import { SiGmail, SiLinkedin, SiGithub } from "react-icons/si";
+import "./about.css"
 
 const About = () => {
   return (
     <>
-    
-    <AboutStyledDiv className="d-flex flex-row align-items-center">
-      <Card style={{ width: "80vw", height:"70vh", margin:"1rem 7rem", display:"flex", flexDirection:"column", alignItems:"center", }}>
-      <AboutStyledH2>About Software Developer</AboutStyledH2>
-        <Card.Img variant="top" src={coding} style={{width:"50%"}}/>
-        <Card.Body style={{display:"flex", flexDirection:"column", justifyContent:"center", height:"100%"}}>
-          <Card.Title className="text-center">Kerim Mustafa DEMİR</Card.Title>
-          <Card.Text style={{height:"100%"}}>
-            <p> Hi everyone 👋 </p>
-            <p> I'm Kerim. </p>
-            <p> I'm interested in Full Stack Development and continue to learn languages in this field. </p>
-            <p>You can contact me from the following links.</p>
-            <p>Happy Coding...</p>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </AboutStyledDiv>
+      <AboutStyledDiv className="d-flex flex-row align-items-center">
+        <Card
+          style={{ width: "80vw", minWidth: "30.06rem", height: "75%", maxHeight: "50.125rem", margin: "1rem 7rem", display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <AboutStyledH2>About Software Developer</AboutStyledH2>
+          <Card.Img variant="top" src={coding} style={{ width: "50%", maxWidth: "35rem", minWidth: "20rem" }} />
+          <Card.Body style={{ display: "flex", flexDirection: "column", justifyContent: "center", height: "100%" }}>
+            <Card.Title style={{fontSize:"1.7rem"}} className="font-weight-bold text-center mt-2 mb-3">Kerim Mustafa DEMİR</Card.Title>
+            <Card.Text style={{ height: "100%", textAlign:"left" }}>
+              <p> Hi everyone 👋 </p>
+              <p> I'm Kerim. </p>
+              <p>
+                {" "}
+                I'm interested in Full Stack Development and continue to learn
+                languages in this field.{" "}
+              </p>
+              <p>You can contact me from the following links.</p>
+              <p>Happy Coding...</p>
+              <AboutStyledContactLinks>
+                <a href="mailto:kerimmstfdemir@gmail.com">
+                  <SiGmail className="logo-gmail logo" style={{ color: "#c5221f", cursor: "pointer" }} />
+                </a>
+                <a href="https://www.linkedin.com/in/krmmstfdemir/" target="blank">
+                  <SiLinkedin className="logo-linkedin logo" style={{ color: "#2868b2", cursor: "pointer" }} />
+                </a>
+                <a href="https://github.com/kerimmstfdemir" target="blank">
+                <SiGithub className="logo-gitgub logo" style={{ color:"#000", cursor: "pointer" }} />
+                </a>
+              </AboutStyledContactLinks>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </AboutStyledDiv>
     </>
   );
 };
--- recipeapp/src/pages/about/About.styled.jsx
+++ recipeapp/src/pages/about/About.styled.jsx
@@ -11,3 +11,11 @@ export const AboutStyledH2 = styled.h2`
   text-align: center;
   padding: 2rem 2rem 1rem 2rem;
 `;
+
+export const AboutStyledContactLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1.3rem;
+  font-size: x-large;
+`
